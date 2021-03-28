1b2ab666620b0bfbc7fa25a071d66dceb63e7d7e
Daniel Lewis <daniel.lewis@protonmail.com>
2021-03-28 15:49:56 -0600

    added title and description prompts

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a description of the project (Required)',
+                validate: descriptionInput => {
+                    if (descriptionInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description!');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Installation
+            },
+            {
+                // Usage
+            },
+            {
+                // Contributing
+            },
+            {
+                // Tests
+            }
+        ]);
+};
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
-init();
+promptUser();
