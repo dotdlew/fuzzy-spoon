145ef3a77f170a19fd2f2ab924ed482374aa1c37
Daniel Lewis <daniel.lewis@protonmail.com>
2021-03-28 14:38:48 -0600

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# fuzzy-spoon
-9. Node.js Challenge Professional README Generator
+# Professional README Generator Starter Code
+
+[How to create a Professional README](./readme-guide.md)
--- index.js
+++ index.js
@@ -1,79 +1,13 @@
-const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+// TODO: Include packages needed for this application
 
-// const pageHTML = generatePage(name, github);
+// TODO: Create an array of questions for user input
+const questions = [];
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+// TODO: Create a function to initialize app
+function init() {}
 
-const promptProject = () => {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)'
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ]);
-};
-
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
-  ]);
-};
-
-promptUser()
-  .then(answers => console.log(answers))
-  .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+// Function call to initialize app
+init();
